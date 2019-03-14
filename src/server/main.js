4612f4ceb3ee716704046fc8aca0ad2d55b93790
Nikola Rusakov <nikola.rusakov@flowup.cz>
2019-03-14 16:11:24 +0100

    local functionality

--- src/server/main.js
+++ src/server/main.js
@@ -29,12 +29,4 @@ app.use(
 );
 
 // Listen for incoming HTTP requests
-const listener = app.listen(process.env.PORT || undefined, () => {
-  let host = listener.address().address;
-  if (host === '::') {
-    host = 'localhost';
-  }
-  const port = listener.address().port;
-  // eslint-disable-next-line no-console
-  console.log('Listening at http://%s%s', host, port === 80 ? '' : ':' + port);
-});
+const listener = app.listen(8888);
